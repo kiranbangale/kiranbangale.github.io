bb169a1f5a0fe2054e93890e5d4d2eafdb8c435b
Kiran Bangale <kiranbangale@gmail.com>
2019-01-30 22:58:53 +0530

    Updates on CV Request

--- portfolio/src/app/shared/data.service.ts
+++ portfolio/src/app/shared/data.service.ts
@@ -11,24 +11,10 @@ export class DataService {
   }
 
   storeFeedback(feedback) {
-    // createUser(value, avatar){
-    //   return this.db.collection('users').add({
-    //     name: value.name,
-    //     nameToSearch: value.name.toLowerCase(),
-    //     surname: value.surname,
-    //     age: parseInt(value.age),
-    //     avatar: avatar
-    //   });
-    // }
-
     return this.db.list('/data/feedbacks').push(feedback);
+  }
 
-    // console.log(feedbackData);
-    // const req = new HttpRequest(
-    //   'PUT',
-    //   'https://ng-recipe-book-3adbb.firebaseio.com/recipes.json',
-    //   feedbackData
-    // );
-    // return this.httpClient.request(req);
+  requestCv(requestCvObj) {
+    return this.db.list('/cvRequested').push(requestCvObj);
   }
 }
